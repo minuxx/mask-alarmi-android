39c2a91f4d92d8f3fd0cb2e58b801282b0863c50
minuxx <minux8905@gmail.com>
2023-08-08 16:38:24 +0900

    chore: app gradle, manifest 완전 초기화

--- app/src/main/java/com/minux/mask_alarmi/MaskAlarmiApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.minux.mask_alarmi
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MaskAlarmiApplication: Application() {
-}
--- app/src/main/java/com/minux/mask_alarmi/core/util/UiEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.minux.mask_alarmi.core.util
-
-sealed class UiEvent {
-    data class ShowToast(val message: String): UiEvent()
-    data class Navigate(val flag: String): UiEvent()
-}
