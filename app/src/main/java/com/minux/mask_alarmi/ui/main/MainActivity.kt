1e831ac3178d27c7b0d3f4b77ad11892e6b1cdee
minuxx <minux8905@gmail.com>
2023-08-08 20:42:59 +0900

    feat: Main 모듈 초기화

---
+++ app/src/main/java/com/minux/mask_alarmi/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.minux.mask_alarmi.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.minux.mask_alarmi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
