029d74331fd19c5a8b08995a43160aabc4c3c0ee
minuxx <minux8905@gmail.com>
2023-08-08 20:42:20 +0900

    feat: Splash 모듈 초기화

---
+++ app/src/main/java/com/minux/mask_alarmi/ui/splash/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.minux.mask_alarmi.ui.splash
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.minux.mask_alarmi.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+        }, 2000)
+    }
+}
