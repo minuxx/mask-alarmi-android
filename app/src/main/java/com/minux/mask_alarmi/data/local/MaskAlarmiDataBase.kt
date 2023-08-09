bd53ffea0de02d8a46097926ac927202fabe77a8
minuxx <minux8905@gmail.com>
2023-08-09 15:47:35 +0900

    add: 마스크알라미 DB, Dao 초기화 in StoreRepository

--- app/src/main/java/com/minux/mask_alarmi/data/local/MaskAlarmiDataBase.kt
+++ app/src/main/java/com/minux/mask_alarmi/data/local/MaskAlarmiDataBase.kt
@@ -4,6 +4,8 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.minux.mask_alarmi.data.model.Store
 
+const val MASK_ALARMI_DB_NAME = "mask-alarmi-database"
+
 @Database(entities = [ Store::class ], version = 1)
 abstract class MaskAlarmiDataBase : RoomDatabase() {
     abstract fun storeDao(): StoreDao
--- app/src/main/java/com/minux/mask_alarmi/data/repository/StoreRepository.kt
+++ app/src/main/java/com/minux/mask_alarmi/data/repository/StoreRepository.kt
@@ -1,8 +1,19 @@
 package com.minux.mask_alarmi.data.repository
 
 import android.content.Context
+import androidx.room.Room
+import com.minux.mask_alarmi.data.local.MASK_ALARMI_DB_NAME
+import com.minux.mask_alarmi.data.local.MaskAlarmiDataBase
 
 class StoreRepository private constructor(context: Context){
+    private val maskAlarmiDB: MaskAlarmiDataBase = Room.databaseBuilder(
+        context.applicationContext,
+        MaskAlarmiDataBase::class.java,
+        MASK_ALARMI_DB_NAME,
+    ).build()
+
+    private val storeDao = maskAlarmiDB.storeDao()
+
     companion object {
         private var INSTANCE: StoreRepository? = null
 
