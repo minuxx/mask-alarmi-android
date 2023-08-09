05ec78da91f03b1a304688260e480052375df9e4
minuxx <minux8905@gmail.com>
2023-08-09 15:22:28 +0900

    add: Store 데이터 클래스 추가

---
+++ app/src/main/java/com/minux/mask_alarmi/data/Store.kt
@@ -0,0 +1,9 @@
+package com.minux.mask_alarmi.data
+
+data class Store (
+    val addr: String,
+    val created_at: String,
+    val name: String,
+    val store_type: Int,
+    val remain_stat: String
+)
