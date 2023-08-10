a6f32d95f9c7a1d4b959fde3b8e0c092463df9b9
minuxx <minux8905@gmail.com>
2023-08-10 10:51:26 +0900

    add: 좌표 사이 거리 계산 함수를 가진 GeoUtil 객체 선언

--- app/src/main/java/com/minux/mask_alarmi/data/repository/StoreRepositoryImpl.kt
+++ app/src/main/java/com/minux/mask_alarmi/data/repository/StoreRepositoryImpl.kt
@@ -4,18 +4,10 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.room.Room
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import com.minux.mask_alarmi.data.entity.StoreEntity
 import com.minux.mask_alarmi.data.local.MASK_ALARMI_DB_NAME
 import com.minux.mask_alarmi.data.local.MaskAlarmiDataBase
 import com.minux.mask_alarmi.domain.model.Store
 import com.minux.mask_alarmi.domain.repository.StoreRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.io.IOException
-import java.io.InputStream
-import java.nio.charset.Charset
 
 
 class StoreRepositoryImpl private constructor(val context: Context) : StoreRepository {
@@ -36,37 +28,11 @@ class StoreRepositoryImpl private constructor(val context: Context) : StoreRepos
         val storeListLiveData = MediatorLiveData<List<Store>>()
 
         storeListLiveData.addSource(stores) { stores ->
-            val transformedList = stores.map { it.toModel() }
-            storeListLiveData.value = transformedList
+            storeListLiveData.value = stores.map { it.toModel() }
         }
 
         return storeListLiveData
     }
-
-    suspend fun insertStoresFromJson() {
-        withContext(Dispatchers.IO) {
-            val json = loadJsonFromAsset()
-            val listType = object : TypeToken<List<StoreEntity>>() {}.type
-            val stores: List<StoreEntity> = Gson().fromJson(json, listType)
-            storeDao.insertStores(stores)
-        }
-    }
-
-    private fun loadJsonFromAsset(): String? {
-        var json: String? = null
-        try {
-            val inputStream: InputStream = context.assets.open("sample_data.json")
-            val size: Int = inputStream.available()
-            val buffer = ByteArray(size)
-            inputStream.read(buffer)
-            inputStream.close()
-            json = buffer.toString(Charset.defaultCharset())
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        return json
-    }
-
 
     companion object {
         private var INSTANCE: StoreRepositoryImpl? = null
@@ -82,3 +48,29 @@ class StoreRepositoryImpl private constructor(val context: Context) : StoreRepos
         }
     }
 }
+
+
+/*
+suspend fun insertStoresFromJson() {
+    withContext(Dispatchers.IO) {
+        val json = loadJsonFromAsset()
+        val listType = object : TypeToken<List<StoreEntity>>() {}.type
+        val stores: List<StoreEntity> = Gson().fromJson(json, listType)
+        storeDao.insertStores(stores)
+    }
+}
+
+private fun loadJsonFromAsset(): String? {
+    var json: String? = null
+    try {
+        val inputStream: InputStream = context.assets.open("sample_data.json")
+        val size: Int = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+        json = buffer.toString(Charset.defaultCharset())
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+    return json
+}*/
